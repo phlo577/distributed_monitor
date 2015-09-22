5eb0404f930f2d504c8fb0498f8525407a9b33b7
phlo577 <florincotoranu@yahoo.com>
2015-09-22 11:53:12 +0300

    Update after feedback

--- agent/main.py
+++ agent/main.py
@@ -1,140 +1,87 @@
-import sys
-import pika
-import logging
-import time
-from config import OS, LABEL, USER_CFG, PASW_CFG, IP_CFG, PORT_CFG
+"""
+    Agent module
+"""
 
-if OS == 'Windows':
-    # For Windows, use WMI
-    import wmi    
-elif OS == 'Linux':
-    # For Linux, use psutil
-    import psutil
-    
-    # Variables used to calcualte disk iops
-    old_time = time.time()
-    old_read_bytes = psutil.disk_io_counters().read_bytes
-    old_write_bytes = psutil.disk_io_counters().write_bytes
-    
-else:
-    print("Invalid OS label")
-    sys.exit()
-
-        
-def get_linux_statistics(label):
-    result = []
-    global old_time
-    global old_read_bytes
-    global old_write_bytes
-    
-    if label == 'cpu_usage':
-        # Get CPU usage of each core
-        percent_list = psutil.cpu_percent(percpu=True)
-        for cpu_percent in percent_list:
-            result.append((str)((int)(cpu_percent)))
-                        
-    elif label == 'memory':
-        # Get total and total available virtual memory
-        memory = psutil.virtual_memory()
-        result = [(str)(memory.total), (str)(memory.available)]
-
-    elif label == 'disk_iops':
-        # Calculate iops, because psutil only provides bytes read and written since startup
-        disk = psutil.disk_io_counters()
-        current_time = time.time()
-
-        read_bytes_sec = (int)((disk.read_bytes - old_read_bytes)/(current_time - old_time))
-        write_bytes_sec = (int)((disk.write_bytes - old_write_bytes)/(current_time - old_time))
-        old_write_bytes = disk.write_bytes
-        old_read_bytes = disk.read_bytes
-        old_time = current_time
-        
-        result = [(str)(read_bytes_sec), (str)(write_bytes_sec)]
-
-    else:
-        # Get total network bytes sent/received
-        network = psutil.net_io_counters()
-        result = [(str)(network.bytes_sent), (str)(network.bytes_recv)]
-
-    return result
-
-def get_windows_statistics(label):
-    c = wmi.WMI()
-    result = []
-    if label == 'cpu_usage':
-        # Get CPU usage of each core
-        for cpu in c.Win32_Processor():
-            result.append((str)(cpu.LoadPercentage))
-            
-    elif label == 'memory':
-        # Calculate total and total available virtual memory
-        total_memory = 0
-        available_memory = 0
-        for os in c.Win32_OperatingSystem():
-            total_memory += (int)(os.TotalVirtualMemorySize)
-            available_memory += (int)(os.FreeVirtualMemory)
-        result = [(str)(total_memory), (str)(available_memory)]
-        
-    elif label == 'disk_iops':
-        # Get total disk byres read/written per second
-        disk = c.Win32_PerfFormattedData_PerfDisk_LogicalDisk()
-        result = [(str)(disk[0].DiskReadBytesPerSec), (str)(disk[0].DiskWriteBytesPerSec)]
-        
-    else:
-        # Calculate total network bytes sent/received
-        bytes_sent = 0
-        bytes_received = 0
-        for network in c.Win32_PerfRawData_Tcpip_NetworkInterface():
-            bytes_sent += (int)(network.BytesSentPerSec)
-            bytes_received += (int)(network.BytesReceivedPerSec)
-        result = [(str)(bytes_sent), (str)(bytes_received)]
-
-    return result
+from pika import PlainCredentials, ConnectionParameters, BlockingConnection
+from logging import basicConfig, CRITICAL
+from datetime import datetime
+from metric import Metric
+from socket import gethostname
 
 
-def on_request(channel, method, props, body):
-    
-    print '[<] Received request for ' + body
-    if OS == 'Windows':
-        # Get statistics for Windows using WMI
-        response = get_windows_statistics(body)            
-    else:
-        # Get statistics from Linux using psutil
-        response = get_linux_statistics(body)
+class Agent(object):
+    """Agent class"""
+    def __init__(self, user, password, ip, port, metric_labels):
+        """Generate metrics list, connect to RabbitMQ server"""
+        # Add valid metrics to list
+        self.metric_list = []
+        for label in metric_labels:
+            metric = Metric.create(label)
+            if metric is not None:
+                self.metric_list.append(metric)
 
-    # Publish response to server
-    channel.basic_publish(exchange='',
-                         routing_key=props.reply_to,
-                         properties=pika.BasicProperties(correlation_id = \
-                                                         props.correlation_id),
-                         body=str(response))
+        # Connect to RabbitMQ server
+        credentials = PlainCredentials(user,
+                                       password)
+        basicConfig(format='%(levelname)s:%(message)s',
+                    level=CRITICAL)
+        connection_parameters = ConnectionParameters(ip,
+                                                     port,
+                                                     '/',
+                                                     credentials)
+        self.connection = BlockingConnection(connection_parameters)
+        self.channel = self.connection.channel()
+        self.host = gethostname()
 
-    channel.basic_ack(delivery_tag = method.delivery_tag)
-    print '[>] Sent statistics for ' + body
+        # Prepare request queue
+        self.processing_request = False
+        self.channel.exchange_declare(exchange='request',
+                                      type='fanout')
+        result = self.channel.queue_declare(exclusive=True)
+        request_queue = result.method.queue
+        self.channel.queue_bind(exchange='request',
+                                queue=request_queue)
+        self.channel.basic_consume(self.request_metric,
+                                   queue=request_queue,
+                                   no_ack=True)
 
+        # Prepare reply queue
+        self.channel.queue_declare(queue='reply')
 
-def main():
+    def request_metric(self, channel, method, properties, body):
+        """Request local metrics and send them to the controller"""
+        print 'Request received'
+        if not self.processing_request:
+            self.processing_request = True
+            for metric in self.metric_list:
+                # Get current time, host and metric
+                metric_type = metric.get_type()
+                value = metric.get_value()
 
-    # Connect to RabbitMQ server and start consuming
-    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.CRITICAL)
-    credentials = pika.PlainCredentials(USER_CFG, PASW_CFG)
-    connection = pika.BlockingConnection(pika.ConnectionParameters(
-                        IP_CFG,
-                        PORT_CFG,
-                        '/',
-                        credentials))
-    channel = connection.channel()
-    channel.queue_declare(LABEL)
-    channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(on_request, queue=LABEL)
-    try:
-        print '[.] Awaiting requests'
-        channel.start_consuming()
-        
-    except KeyboardInterrupt:
-        print 'STOP'
-        channel.stop_consuming()
-        connection.close()
+                timestamp = [datetime.now().year,
+                             datetime.now().month,
+                             datetime.now().day,
+                             datetime.now().hour,
+                             datetime.now().minute,
+                             datetime.now().second,
+                             0]
 
-if __name__ == "__main__":
-    main()
+                # Send reply to controller
+                reply = str([timestamp, self.host, metric_type, value])
+                self.channel.basic_publish(exchange='',
+                                           routing_key='reply',
+                                           body=reply)
+                print 'Metric ' + metric_type + ' sent'
+            self.processing_request = False
+
+    def start_consuming(self):
+        """Start consuming request messages"""
+        self.channel.start_consuming()
+
+    def stop_consuming(self):
+        """Stop consuming request messages"""
+        self.channel.stop_consuming()
+
+    def disconnect(self):
+        """Disconnect controller from RabbitMQ server"""
+        self.connection.close()
